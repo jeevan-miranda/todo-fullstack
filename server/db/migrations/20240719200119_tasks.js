bc7cf6d8803d2c6ff479f1aa35f578b765121f1c
Jeevan Miranda <jeevan_miranda@hotmail.com>
2024-07-20 08:06:10 +1200

    tasks db migration added

---
+++ server/db/migrations/20240719200119_tasks.js
@@ -0,0 +1,21 @@
+import e from 'express'
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('tasks', (table) => {
+    table.increments('id')
+    table.string('task')
+    table.boolean('completed)')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('tasks')
+}
