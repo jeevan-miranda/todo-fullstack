154538b3272e2e44260a3c7f875b83a24a069b24
Jeevan Miranda <jeevan_miranda@hotmail.com>
2024-07-20 09:00:19 +1200

    getalltasks and gettasksbyid db functions added

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,5 +1,17 @@
+import { Task } from '../../models/tasks'
 import connection from './connection'
 
-export async function getAllTasks(): Task[] {
+export async function getAllTasks(): Promise<Task[]> {
   return await connection('tasks').select()
 }
+
+//getTaskById
+export async function getTaskById(id: number): Promise<Task> {
+  return await connection('tasks').where({ id }).first()
+}
+
+//addTask
+
+//editTask
+
+//deleteTask
